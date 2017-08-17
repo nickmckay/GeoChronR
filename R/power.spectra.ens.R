40d3672232abb304275506dcd1cb6295bb8ba175
Julien Emile-Geay <julieneg@usc.edu>
2017-08-17 12:41:49 -0700

    AR(1) generation
    renamed createSyntheticSeries to ar1Surrogates
    made AR(1) exclusive
    detrending is optional, but done by default

--- R/power.spectra.ens.R
+++ R/power.spectra.ens.R
@@ -6,9 +6,10 @@
 #' @description create synthetic timeseries based on a timeseries. Useful for null hypothesis testing
 #' @param time LiPD "variable list" or vector of year/age values
 #' @param vals LiPD "variable list" or vector of values
+#' @param detrend_bool boolean variable, indicating whether one should remove trend (default) or not. Note: does not affect the AR(1) fit, but does affect the generated timeseries
 #' @param nens Number of ensemble members to simulate
 #' @return a vector or matrix of AR(1) surrogates for series X
-ar1Surrogates = function(time,vals,detrend_bool=FALSE,nens=1){
+ar1Surrogates = function(time,vals,detrend_bool=TRUE,nens=1){
   
   #check to see if time and values are "column lists"
   if(is.list(time)){time=time$values}
@@ -22,38 +23,30 @@ ar1Surrogates = function(time,vals,detrend_bool=FALSE,nens=1){
   tnai = which(is.na(time))
   vnai = which(is.na(vals))
   
-  # if detrend option is passed, use detrended vvalues; otherwise, unchanged.
+  trend=predict(lm(vals~time))     #fit a linear trend
+  # if detrend option is passed, use detrended values; otherwise, unchanged.
   if(detrend_bool){
-    #fit a linear trend
-    trend=predict(lm(vals~time))
     #remove the linear trend
     vals_used=vals-trend
   } else {vals_used=vals}
     
-  #get necessary metadata
+  #extract low-order moments
   m=mean(vals_used,na.rm=TRUE)
   s=sd(vals_used,na.rm=TRUE)
   
   fit = arima(x = vals_used, order = c(1, 0, 0))
-  #a=acf(vals_used,na.action=na.pass,plot=FALSE)
-  #ar=max(0,as.numeric(unlist(a[1])[1]))
   
   vals_syn = matrix(NA,nrow=nrow(time),ncol=nens)
-  #go through ensemble members
-  for(jj in 1:nens){
-    #generate a random series with ar=ar
+ 
+  for(jj in 1:nens){  #go through ensemble members
+    #generate a random series with fitted model parameters
     ar1=arima.sim(model=fit$model,n=length(vals_used))
-    #remove any trend
-    #rtrend=predict(lm(ar1~time))
-    #ar1=ar1-rtrend
-    
-    #scale the same as data
-    ar1=scale(ar1)*s+m
+    ar1=scale(ar1)*s+m   #scale the same as original values
     ar1[vnai]=NA # put NA values in same place as original
-    vals_syn[,jj]=ar1
+    vals_syn[,jj]=ar1 
   }
-  # if trend was taken out, add it back in to each ensemble member
-  if(detrend_bool){
+  # if trend needs to stay in, add it back in
+  if(!detrend_bool){
     vals_syn = vals_syn + replicate(nens,trend)
   }
   return(vals_syn)
@@ -74,7 +67,7 @@ ar1Surrogates = function(time,vals,detrend_bool=FALSE,nens=1){
 #' \item powerSyn: matrix of synthetic spectral power results
 #' }
 #' @import lomb
-powerSpectrumEns = function(time,values,spc_mthd = 'Lomb-Scargle',max.ens=NA,ofac=1){
+powerSpectrumEns = function(time,values,max.ens=NA,ofac=1){
   #check to see if time and values are "column lists"
   if(is.list(time)){time=time$values}
   if(is.list(values)){values=values$values}
@@ -106,10 +99,10 @@ powerSpectrumEns = function(time,values,spc_mthd = 'Lomb-Scargle',max.ens=NA,ofa
   
   pb = txtProgressBar(min=1,max = nens,style = 3)
   for(i in 1:nens){
-    t = time[,sample.int(ncol(time),size = 1)]
+    t = time[,sample.int(ncol(time),size = 1)]  # random sampling 
     v = values[,sample.int(ncol(values),size = 1)]
     out = lsp(v,times=t,ofac=ofac,plot = F)
-    syn = ar1Surrogates(t,v)#create synthetic timeseries  #JEG: this should be done outside the loop
+    syn = ar1Surrogates(t,v)#create synthetic timeseries  
     synOut = lsp(syn,times=t,ofac=ofac,plot = F) 
     pMatSyn[1:length(synOut$power),i] =  synOut$power
     fMat[,i]=out$scanned
