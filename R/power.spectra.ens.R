1364a94e37dd6b4dc46e412f0acd710bebf1e36d
Julien Emile-Geay <julieneg@usc.edu>
2017-08-17 17:43:17 -0700

    isopersist + AR(1) generation
    added corrIsopersist to correlation.regression.functions.R;  computes
    p- value wrt to empirical distribution of correlations to AR(1)
    timeseries
    
    in power.spectra.ens.R, added ar1Surrogates, which (1) fits only AR(1)
    series; (2) does so using redfit (via dplR) so it can handle unevenly
    spaced data.

--- R/power.spectra.ens.R
+++ R/power.spectra.ens.R
@@ -1,15 +1,72 @@
 #' @export
 #' @family spectra
 #' @family pca
+#' @title Create a synthetic timeseries that emulates the characteristics of a variable
+#' @description create synthetic timeseries based on a timeseries. Useful for null hypothesis testing
+#' @param time LiPD "variable list" or vector of year/age values
+#' @param values LiPD "variable list" or vector of values
+#' @param nens Number of ensemble members to simulate
+#' @return a vector or matrix of synthetic values
+createSyntheticTimeseries = function(time,values,nens=1){
+  
+  #check to see if time and values are "column lists"
+  if(is.list(time)){time=time$values}
+  if(is.list(values)){values=values$values}
+  
+  #make them all matrices
+  time = as.matrix(time)
+  values = as.matrix(values)
+  
+  #find the NAs...
+  tnai = which(is.na(time))
+  vnai = which(is.na(values))
+  
+  
+  
+  #measure long term trend
+  trend=predict(lm(values~time))
+  #remove the trend
+  notrend=values-trend
+  #get necessary metadata
+  m=mean(notrend,na.rm=TRUE)
+  s=sd(notrend,na.rm=TRUE)
+  
+  fit = arima(x = notrend, order = c(1, 0, 0))
+  #a=acf(notrend,na.action=na.pass,plot=FALSE)
+  #ar=max(0,as.numeric(unlist(a[1])[1]))
+  
+  synValues = matrix(NA,nrow=nrow(time),ncol=nens)
+  #go through ensemble members
+  for(jj in 1:nens){
+    #generate a random series with ar=ar
+    rdata=arima.sim(model=fit$model,n=length(notrend))
+    #remove any trend
+    rtrend=predict(lm(rdata~time))
+    rdata=rdata-rtrend
+    #scale the same as data
+    rdata=scale(rdata)*s+m
+    #add the data trend in
+    withTrend=rdata+trend
+    withTrend[vnai]=NA
+    synValues[,jj]=withTrend
+  }
+  return(synValues)
+}
+
+#' @export
+#' @import dplR
+#' @family spectra
+#' @family pca
 #' @family correlations
 #' @title Create a synthetic timeseries that emulates the characteristics of a variable
 #' @description create synthetic timeseries based on a timeseries. Useful for null hypothesis testing
 #' @param time LiPD "variable list" or vector of year/age values
 #' @param vals LiPD "variable list" or vector of values
 #' @param detrend_bool boolean variable, indicating whether one should remove trend (default) or not. Note: does not affect the AR(1) fit, but does affect the generated timeseries
+#' @param method Method used for estimation. Possible values are "even" or "redfit"
 #' @param nens Number of ensemble members to simulate
 #' @return a vector or matrix of AR(1) surrogates for series X
-ar1Surrogates = function(time,vals,detrend_bool=TRUE,nens=1){
+ar1Surrogates = function(time,vals,detrend_bool=TRUE,method='redfit',nens=1){
   
   #check to see if time and values are "column lists"
   if(is.list(time)){time=time$values}
@@ -33,14 +90,20 @@ ar1Surrogates = function(time,vals,detrend_bool=TRUE,nens=1){
   #extract low-order moments
   m=mean(vals_used,na.rm=TRUE)
   s=sd(vals_used,na.rm=TRUE)
-  
-  fit = arima(x = vals_used, order = c(1, 0, 0))
+  if (method=='redfit') {
+    redf.dat <- redfit(vals_used, time, nsim = 21) # 21 is minimum numer you can get away with
+    g = redf.dat$rho
+  } else {
+    fit = arima(x = vals_used, order = c(1, 0, 0)) # assumes evenly-spaced data
+    g = as.numeric(fit$coef[1])
+  }
   
   vals_syn = matrix(NA,nrow=nrow(time),ncol=nens)
  
   for(jj in 1:nens){  #go through ensemble members
     #generate a random series with fitted model parameters
-    ar1=arima.sim(model=fit$model,n=length(vals_used))
+    ar1=arima.sim(model=list(g),n=length(vals_used))
+    
     ar1=scale(ar1)*s+m   #scale the same as original values
     ar1[vnai]=NA # put NA values in same place as original
     vals_syn[,jj]=ar1 
