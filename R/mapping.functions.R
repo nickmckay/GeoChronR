40b53032aac546d134fc3e8012f2fb6b00ed9968
Nick McKay <nickmckay@users.noreply.github.com>
2018-01-25 17:04:07 -0700

    improved stereographic line mapping

--- R/mapping.functions.R
+++ R/mapping.functions.R
@@ -73,21 +73,34 @@ baseMap = function(lon,lat,map.type="google",f=.3,restrict.map.range=TRUE,projec
     lat = lat + c(-extend.range,extend.range)
   }
   
+  #boundcirc and restrict.map.range can't both be true
+  if(boundcirc & restrict.map.range){
+    warning("boundcirc and restrict.map.range can't both be true - setting restrict.map.range to FALSE")
+    restrict.map.range=FALSE
+  }
   
-  try(load("bound.box.Rdata"),silent = TRUE)
-  if(!exists("bb")){bb=1000}
+  if(map.type=="google"){
+    try(load("bound.box.Rdata"),silent = TRUE)
+    if(!exists("bb")){bb=1000}
+  }else{
+    bb=1000
+  }
   
-  bbnew <- make_bbox(lon,lat,f=f)
+  bbnew <- ggmap::make_bbox(lon,lat,f=f)
   if(all(bbnew==bb)){
     lnp=TRUE
     #load the file you already downloaded
   }else{
     lnp=FALSE
     bb=bbnew
+    if(map.type=="google"){
     if(bb[4] > 89){
       diff <- bb[4]-89
       bb[2] <- bb[2]-(diff*2)
       bb[4] <- bb[4]-(diff*2)
+    }
+    }else if(bb[4] > 90){
+      bb[4]=90
     }
     #save(bb,file="bound.box.Rdata")
   }
@@ -101,10 +114,12 @@ baseMap = function(lon,lat,map.type="google",f=.3,restrict.map.range=TRUE,projec
     #}
     #  save(newmap,file="newmap.RData")
     baseMap = ggmap(newmap,maprange=TRUE) 
+    
   }else if(map.type=="line"){
     
     res=1
     low.lat <- min(lat)-5
+    
     if(restrict.map.range){
       x_lim = bb[c(1,3)]
       y_lim = bb[c(2,4)]
@@ -112,30 +127,43 @@ baseMap = function(lon,lat,map.type="google",f=.3,restrict.map.range=TRUE,projec
       x_lim = c(-190,190)
       y_lim = c(min(bb[c(2,4)]),90)
     }
+    
     x_cell_lim <- x_lim + c(1, -1) * res/2
     y_cell_lim <- y_lim+ c(1, -1) * res/2
+    
+    
+    
     if(global){
       dum = map(plot = FALSE)
     }else{
       dum = map(xlim = x_lim, ylim = y_lim, plot = FALSE,wrap=TRUE)
     }
+    
     #dum = map(xlim = x_cell_lim, ylim = y_cell_lim, plot = FALSE)
     ant_ggplot = data.frame(dum[c("x","y")])
-    badLines = which(ant_ggplot$x < min(x_lim) | ant_ggplot$x > max(x_lim) | ant_ggplot$y < min(y_lim) | ant_ggplot$y > max(y_lim))
+    if(min(x_lim)<=-180 & max(x_lim)>=180){#don't restrict lon
+      badLines = which(ant_ggplot$y <= min(y_lim) | ant_ggplot$y >= max(y_lim))
+      
+    }else{
+    badLines = which(ant_ggplot$x <= min(x_lim) | ant_ggplot$x >= max(x_lim) | ant_ggplot$y <= min(y_lim) | ant_ggplot$y >= max(y_lim))
+    }
     ant_ggplot[badLines,]=NA
     
     
     bbdf=data.frame(bb)
     
     baseMap <- ggplot() +  geom_path(aes(x = x, y = y), data = ant_ggplot)
+    
     if(restrict.map.range){
       baseMap  = baseMap  +  geom_rect(aes(xmax=bb[3]-.1,xmin=bb[1]+.1,ymax=bb[4]-.1,ymin=bb[2]+.1),fill=NA, colour="black",data=bbdf)
+    
     }else if(boundcirc){
       wbb=which(min(abs(bb[c(2,4)]))==abs(bb[c(2,4)]))
       wbb=c(2,4)[wbb]
       boundcirc = data.frame(x = seq(-180,180),y=rep(bb[wbb]+.1,length.out=length(seq(-180,180))))
       baseMap  = baseMap  + geom_path(aes(x = x, y = y), data = boundcirc)
     }
+    
     baseMap  = baseMap  +   
       theme(panel.background = element_rect(fill="white"), axis.ticks = element_blank(), axis.text.y = element_blank(),axis.text.x = element_blank(),
             axis.title.x = element_blank(), axis.title.y = element_blank(),
