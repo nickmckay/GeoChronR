54d62c0e835a4b3ea9e50accccbc44bb89baed25
Julien Emile-Geay <julieneg@usc.edu>
2020-06-20 16:00:57 -0700

    added redfit to computeSpectraEns
    updated description, tested on a single member. Ensemble mode still needs work.

--- R/power.spectra.ens.R
+++ R/power.spectra.ens.R
@@ -130,11 +130,19 @@ ar1Surrogates = function(time,vals,detrend_bool=TRUE,method='redfit',nens=1){
   return(vals_syn)
 }
 
-
+ 
 #' @export
 #' @family spectra
 #' @title Calculate ensemble power spectra
-#' @description Calculate ensemble power spectra using a variety of spectral methods: Lomb-Scargle, Multi-Taper, and Weighted Wavelet Z-transform
+#' @description Calculate ensemble power spectra using 4 spectral methods: 
+#' \enumerate{
+#' \item Lomb-Scargle periodogram
+#' \item REDFIT (Mudelsee et al, 2009) 
+#' \item Multitaper Method (Thomson et al. 1982)
+#' \item nuspectral (Mathias et al 2004)  (no confidence estimation)
+#' }
+#' For REDFIT only the 95% confidence limit is computed. The estimation thereof is as described in Mudelsee et al 2009 for a single ensemble member.
+#' For multiple ensemble members, the upper 95% quantile of that 95% quantile is taken (a conservative choice).  
 #' @param time LiPD "variable list" or vector of year/age values
 #' @param values LiPD "variable list" or vector of values
 #' @param max.ens Maximum number of ensemble members to analyze
@@ -143,16 +151,20 @@ ar1Surrogates = function(time,vals,detrend_bool=TRUE,method='redfit',nens=1){
 #' @param padfac padding factor for astrochron::mtm___ functions
 #' @param wgtrad radius of the nuspectral::nuwaveletcoeff weight function (non-dimensional units)
 #' @param sigma decay parameter of the nuspectral::nuwaveletcoeff wavelets
+#' @param probs vector of probabilities for the siginificance levels. To avoid such computations, pass `probs = NA`.
 #' @return a list of ensemble spectra results
 #' \itemize{
 #' \item freqs: vector of frequencies
 #' \item power: vector of spectral powers
-#' \item powerSyn: matrix of synthetic spectral power results
+#' \item power.CL: matrix of confidence limits for spectral power
 #' }
 #' @import dplR
 #' @import astrochron
-#' @suggest nuspectral
-computeSpectraEns = function(time,values,max.ens=NA,method='mtm',gauss=FALSE,ofac=4,padfac=2,tbw=3,wgtrad=1,sigma=0.02,mtm_null='power_law'){
+#' @references Mudelsee, M., D. Scholz, R. Röthlisberger, D. Fleitmann, A. Mangini, and E. W. Wolff (2009), Climate spectrum estimation in the presence of timescale errors, Nonlinear Processes in Geophysics, 16(1), 43–56, doi:10.5194/npg-16-43-2009.
+#' @references Mathias, A., F. Grond, R. Guardans, D. Seese, M. Canela, and H. Diebner (2004), Algorithms for spectral analysis of irregularly sampled time series, Journal of Statistical Software, Articles, 11(2), 1–27, doi:10.18637/jss.v011.i02.
+#' @references Thomson, D. J. (1982), Spectrum estimation and harmonic analysis, Proc. IEEE, 70(9), 1055–1096.
+
+computeSpectraEns = function(time,values,max.ens=NA,method='mtm',probs=0.95,gauss=FALSE,ofac=4,padfac=2,tbw=3,wgtrad=1,sigma=0.02,mtm_null='power_law'){
   
   #check to see if time and values are "column lists"
   if(is.list(time)){time=time$values}
@@ -191,6 +203,7 @@ computeSpectraEns = function(time,values,max.ens=NA,method='mtm',gauss=FALSE,ofa
                    times = t,
                    ofac = ofac,
                    plot = F)
+    noutrow = length(lomb.out$power)
     
     if (nens == 1) {
       fMat = lomb.out$scanned
@@ -223,9 +236,14 @@ computeSpectraEns = function(time,values,max.ens=NA,method='mtm',gauss=FALSE,ofa
         t = jitter(time[,tind[k]])   # add jitter
         v = vals[,vind[k]] 
         out = lsp(v,times=t,ofac=ofac,plot = F) # Lomb-Scargle Periodogram
-        syn = ar1Surrogates(time=t,vals = v) #create matrix of synthetic timeseries  
-        synOut = lsp(syn,times=t,ofac=ofac,plot = F) # apply to AR(1) surrogate [NoTE: need to do more than 1...]
-        pMatSyn[1:length(synOut$power),k] =  synOut$power # store for later  
+        if(!is.na(probs)) {
+          syn = ar1Surrogates(time = t, vals = v) #create matrix of synthetic timeseries
+          synOut = lsp(syn,
+                       times = t,
+                       ofac = ofac,
+                       plot = F) # apply to AR(1) surrogate [NoTE: need to do more than 1...]
+          pMatSyn[1:length(synOut$power), k] =  synOut$power # store for later
+        }
         # JEG: the frequency axis will be subtly different for each iteration.  I think it would be better to interpolate to a common frequency axis if we are to compare them all. Or does quantile2d already take care of that?
         fMat[,k]=out$scanned
         pMat[,k]=out$power
@@ -234,48 +252,76 @@ computeSpectraEns = function(time,values,max.ens=NA,method='mtm',gauss=FALSE,ofa
         }
       }
       close(pb)
-    } else if (nens < 1){
-      stop('My dear! You need a LOT more ensemble members. One would be a start.')
-    }
-    # allocate output  
-    spec.ens = list(freqs = fMat,power = pMat, powerSyn = pMatSyn)
+    } 
+    
+    # establish confidence limits
+    pCL = matrix(NA, ncol = length(probs),nrow = noutrow)
+    
+    if(!is.na(probs)) {
+      for(i in 1:noutrow){
+        pCL[i,] = quantile(pMatSyn[i,],probs = probs,na.rm  = T)
+      } 
+      # allocate output  
+      spec.ens = list(freqs = fMat,power = pMat, power.CL = pCL)
+    } else {spec.ens = list(freqs = fMat,power = pMat, power.CL = NA)}
   }
   
   else if (method=='redfit') {
     # make sure the time axis is increasing
-    dt = diff(time[,1])
-    if(median(dt) < 0){time <- rev(time, 1) }
-      
-    # blank run on the first ensemble member to obtain matrix dimensions
-    redfit.out = redfit(vals[,1], time[,1], tType = "time", mctest = FALSE,ofac=ofac)
-    noutrow = length(out.red$freq)
-    
-    #preallocate matrices
-    fMat = matrix(NA,ncol=nens,nrow=noutrow)
-    pMat = fMat
-    pMatSyn = fMat
-    
-    pb = txtProgressBar(min=1,max = nens,style = 3)
-    for(k in 1:nens){
-      t = jitter(time[,tind[k]])   # add jitter
-      v = vals[,vind[k]] 
-      redfit.out = redfit(v, t, tType = "time", mctest = TRUE,ofac=ofac,nsim=100)
-      
-      pMatSyn[,k] = redfit.out$gredth # 95% limit
-      fMat[,k]=out.red$freq
-      pMat[,k]=out.red$gxx
-      if(k%%round(nens/50)==0){
-        setTxtProgressBar(pb,k)
-      }
+    dt = diff(time[, 1])
+    if (median(dt) < 0) {
+      time <- rev(time, 1)
     }
-    close(pb)
+    
+    t = time[, 1]
+    v = vals[, 1]
     
     
-    # allocate output  
-    spec.ens = list(freqs = fMat,power = pMat, powerSyn = pMatSyn)
-  }
-  
-  else if ( method=='mtm') {    # need to make a choice on null : AR(1) or power law?
+    mcflag = !is.na(probs) # only activate Monte Carlo test if probs is not NA
+    
+    # blank run on the first ensemble member to obtain matrix dimensions
+    redfit.out <- redfit(v,
+                        t,
+                        tType = "time",
+                        mctest = mcflag,
+                        ofac = ofac)
+    noutrow = length(redfit.out$freq)
+    
+    if (nens == 1) {
+      freq <-  redfit.out$freq
+      pMat <-  redfit.out$gxx
+      pCL <- redfit.out$ci95
+    }
+    else if (nens > 1) {
+      #preallocate matrices
+      pMat = matrix(NA, ncol = nens, nrow = noutrow)
+      pMatSyn = fMat
+      
+      pb = txtProgressBar(min = 1, max = nens, style = 3)
+      for (k in 1:nens) {
+        t <- jitter(time[, tind[k]])   # add jitter
+        v <- vals[, vind[k]]
+        redfit.out <- redfit(v,t,tType = "time", mctest = mcflag, ofac = ofac, nsim=200)
+        pMatSyn[, k] <- redfit.out$ci95 # 95% limit
+        pMat[, k] <- redfit.out$gxx
+        if (k %% round(nens / 50) == 0) {
+          setTxtProgressBar(pb, k)
+        }
+      }
+      close(pb)
+    } 
+    
+    # establish confidence limits
+    if(!is.na(probs)) {
+      pCL = matrix(NA,ncol = 1,nrow = noutrow)
+      for(i in 1:noutrow){
+        pCL[i,] = quantile(pMatSyn[i,],probs = 0.95,na.rm  = T)
+      } 
+      # allocate output  
+      spec.ens = list(freqs = freq, power = pMat, power.CL = pCL)
+    } else {spec.ens = list(freqs = freq,power = pMat, power.CL = NA)}
+    
+  } else if ( method=='mtm') {    # need to make a choice on null : AR(1) or power law?
     if ( mtm_null=='AR(1)') {
       mtm.func = astrochron::mtm
     } else if ( mtm_null=='power_law') {
@@ -317,10 +363,18 @@ computeSpectraEns = function(time,values,max.ens=NA,method='mtm',gauss=FALSE,ofa
       }
     }
     close(pb)
-    freqs.prob = rowMeans(ens.mtm.sigfreq)
+    #freqs.prob = rowMeans(ens.mtm.sigfreq)
     f = mtm.main$Frequency
+    
+    # establish confidence limits
+    pCL = matrix(NA, ncol = length(probs),nrow = noutrow)
+    
+    for(i in 1:noutrow){
+      pCL[i,] = quantile(pMatSyn[i,],probs = probs,na.rm  = T)
+    } 
+    
     # allocate output
-    spec.ens = list(freqs = matrix(f,nrow=length(f),ncol=nens,byrow=F), power = ens.mtm.power, powerSyn = NA, prob = freqs.prob)
+    spec.ens = list(freqs = matrix(f,nrow=length(f),ncol=nens,byrow=F), power = ens.mtm.power, power.CL=pCL)
     }
   else if ( method=='nuspectral') {
     nt = length(time)
@@ -332,31 +386,32 @@ computeSpectraEns = function(time,values,max.ens=NA,method='mtm',gauss=FALSE,ofa
     noutrow <- length(nuspec$Frequency)
     
     #preallocate matrices
-    fMat = matrix(NA,ncol=nens,nrow=noutrow)
-    pMat = fMat
-    pMatSyn = fMat
+    #fMat = matrix(NA,ncol=nens,nrow=noutrow)
+    #pMat = fMat
+    #pMatSyn = fMat
     
     # 1st, heard-earned ensemble member
-    pMat[,1] <- nuspec$Power
-    fMat[,1] <- nuspec$Frequency
+    pMat <- nuspec$Power
+    freq <- nuspec$Frequency
     
-    pb = txtProgressBar(min=1,max = nens,style = 3)  # run the loop with progress bar
-    for (k in 2:nens){
-      t = time[,tind[k]]; v = vals[,vind[k]] 
-      nuspec   <-  nuspectral:::nuwavelet_psd(t,v,sigma=sigma,taus = tau)
-      pMat[,k] <- nuspec$Power
-      fMat[,k] <- nuspec$Frequency
-  
-      if(k%%round(nens/10)==0){
-        setTxtProgressBar(pb,k)
-      }
-    }
-    close(pb)
+    if (nens > 1){stop("Ensemble mode not supported with nuspectral")}
+    # pb = txtProgressBar(min=1,max = nens,style = 3)  # run the loop with progress bar
+    # for (k in 2:nens){
+    #   t = time[,tind[k]]; v = vals[,vind[k]] 
+    #   nuspec   <-  nuspectral:::nuwavelet_psd(t,v,sigma=sigma,taus = tau)
+    #   pMat[,k] <- nuspec$Power
+    #   fMat[,k] <- nuspec$Frequency
+    # 
+    #   if(k%%round(nens/10)==0){
+    #     setTxtProgressBar(pb,k)
+    #   }
+    # }
+    # close(pb)
     
     # allocate output
-    spec.ens = list(freqs = fMat, power = pMat, powerSyn = NA, prob = NA)
+    spec.ens = list(freqs = freq, power = pMat, power.CL = NA)
     
-  } else {stop("Unknown method: Valid choices are: mtm, nuspectral, or lomb-scargle")}
+  } else {stop("Unknown method: Valid choices are: 'mtm', 'redfit', 'nuspectral', or 'lomb-scargle' ")}
   
   return(spec.ens)
 }
